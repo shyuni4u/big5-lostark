b896bc1f77acdb20a1d722c6cddcd214b01b7567
shyuni4u <shyuniz@naver.com>
2021-06-07 01:26:44 +0900

    fix ui little.

--- components/organisms/Result.tsx
+++ components/organisms/Result.tsx
@@ -54,8 +54,8 @@ const StyledResult = styled.div`
       flex-wrap: wrap;
       align-items: center;
       justify-content: center;
-      margin-bottom: 25px;
-      padding-bottom: 10px;
+      margin-bottom: 10px;
+      padding-bottom: 25px;
       border-bottom: 1px solid ${theme.colors.info};
     `;
   }}
